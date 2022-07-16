4b4c7ede15f72382a54140160c0d99a3474ca284
adrian rodriguez <adrian.rodriguezvega@gmail.com>
2022-07-16 13:08:37 +1000

    Initial commit

---
+++ test/api-using-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ApiUsingCdk from '../lib/api-using-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/api-using-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ApiUsingCdk.ApiUsingCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
