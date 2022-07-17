520020a83ae82b41db3cd9a01c56189a1b31871d
adrian rodriguez <adrian.rodriguezvega@gmail.com>
2022-07-17 21:45:01 +1000

    My first commit

--- lib/api-using-cdk-stack.ts
+++ lib/api-using-cdk-stack.ts
@@ -1,4 +1,7 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -8,9 +11,37 @@ export class ApiUsingCdkStack extends Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApiUsingCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Dynamo table
+    const myDnamoTable = new Table(this, "MyDynamoTable", {
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      partitionKey: {name: "id", type: AttributeType.STRING},
+      tableName: "MyDynamoTable"
+    });
+
+    //Lambda function
+    const myLambdaFunction = new Function(this, "MyLambdaFunction", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset("lambda"),
+      handler: "app.handler",
+      environment:{
+        MY_DYNAMO_TABLE: myDnamoTable.tableName        
+      }
+    });
+
+    //Grant permission for lambda to write and read dymano table
+    myDnamoTable.grantReadWriteData(myLambdaFunction);
+
+    //APIGateway
+    const myApiGateway = new RestApi(this, "MyApiGateway");
+
+    //Integrate Lambda with apigateway
+    const myLambdaApiGatewayIntegration = new LambdaIntegration(myLambdaFunction);
+
+    myApiGateway.root.resourceForPath("myroute").addMethod("GET", myLambdaApiGatewayIntegration);
+
+    new CfnOutput(this, "HTTP API URL", {
+      value: myApiGateway.url ?? "Something went wrong with the deploy",
+    });
+
   }
 }
